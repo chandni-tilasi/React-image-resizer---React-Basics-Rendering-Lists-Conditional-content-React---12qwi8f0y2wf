69a3c6dd39765150565351b1d8c1dd8e64c35454
chandni-tilasi <96279259+chandni-tilasi@users.noreply.github.com>
2023-01-25 11:09:11 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,40 @@
-import React, { useState } from 'react'
-import '../styles/App.css';
-import imgs from "../image"
+import React, { useState } from "react";
+import "../styles/App.css";
+import imgs from "../image.jpeg";
 const App = () => {
-  const [height,setHeight] = useState("320");
-  const [width,setWidth] = useState("320");
-console.log(imgs)
-  function heightFun(e){
+  const [height, setHeight] = useState("320");
+  const [width, setWidth] = useState("320");
+  
+  function heightFun(e) {
     setHeight(e.target.value);
   }
-  function widthFun(e){
+  function widthFun(e) {
     setWidth(e.target.value);
   }
 
-  
-
   return (
     <div id="main">
-      {/* <img src='' /> */}
-    <input  id="height-slider" type="range" value={height} onChange={heightFun} min={100} max={800} />
-    <input id="width-slider" type="range" value={width} onChange={widthFun}  min={100} max={800} />
+      <img id="resizable-image" src={imgs} width={width} height={height} />
+      {/* <h1> {imgs} </h1> */}
 
+      <input
+        id="height-slider"
+        type="range"
+        value={height}
+        onChange={heightFun}
+        min={100}
+        max={800}
+      />
+      <input
+        id="width-slider"
+        type="range"
+        value={width}
+        onChange={widthFun}
+        min={100}
+        max={800}
+      />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
