a213e7e8344009535392ac5c1f28435c2c8b79de
chandni-tilasi <96279259+chandni-tilasi@users.noreply.github.com>
2023-01-25 05:15:12 +0000

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
-
+import imgs from "../image"
 const App = () => {
+  const [height,setHeight] = useState("320");
+  const [width,setWidth] = useState("320");
+console.log(imgs)
+  function heightFun(e){
+    setHeight(e.target.value);
+  }
+  function widthFun(e){
+    setWidth(e.target.value);
+  }
+
+  
+
   return (
     <div id="main">
+      {/* <img src='' /> */}
+    <input  id="height-slider" type="range" value={height} onChange={heightFun} min={100} max={800} />
+    <input id="width-slider" type="range" value={width} onChange={widthFun}  min={100} max={800} />
 
     </div>
   )
